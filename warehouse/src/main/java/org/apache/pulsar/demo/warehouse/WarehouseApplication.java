b3d16960604862f570a890b5214e76f01bd72dab
Julien Jakubowski <jja@Juliens-MBP.lan>
2022-12-06 23:38:21 +0100

    first commit

---
+++ warehouse/src/main/java/org/apache/pulsar/demo/warehouse/WarehouseApplication.java
@@ -0,0 +1,13 @@
+package org.apache.pulsar.demo.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseApplication.class, args);
+	}
+
+}
---
+++ website/src/test/java/org/apache/pulsar/demo/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.apache.pulsar.demo.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
